1b516ad36849b1294477ef62e3d9fadb4d70fd02
Adam Hardy <ahardy313@gmail.com>
2020-02-05 14:11:20 -0700

    Add project files.

---
+++ Project1/q.h
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+//Queue item structure. Contains a payload and next and previous pointers
+struct element {
+	int payload = 0;
+	element *next;
+	element *prev;
+};
+
+//Creates a new item. Allocates space for the item and returns it.
+element *newItem() {
+	struct element *newItem = (struct element*)malloc(sizeof(struct element));
+
+	return newItem;
+}
+
+//Initializes a queue
+void initQueue(element *head) {
+	head = NULL; //Head is set to NULL since queue is empty
+}
+
+//Adds item to front of queue
+void addQueue(element *head, element *item) {
+	if (head != NULL) { //If head not NULL
+		if (head->next != NULL) { //Check if other element exists
+			item->next = head->next; //If so, next of new element is original second element
+		}
+		head->next = item;
+		item->prev = head;		
+	}
+	else {
+		head = item;
+	}
+}

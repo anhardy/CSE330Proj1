3957c001827237aadbab6e6235c04e8ddadc4209
Adam Hardy <ahardy313@gmail.com>
2020-02-06 13:45:45 -0700

    addQueue() fully defined. delQueue() fully defined. rotateQueue() fully defined. Needs testing.

--- Project1/q.h
+++ Project1/q.h
@@ -19,7 +19,32 @@ void initQueue(element *head) {
 	head = NULL; //Head is set to NULL since queue is empty
 }
 
-//Adds item to front of queue
+//Add item to front of queue
 void addQueue(element *head, element *item) {
-	
+	if (head == NULL) { //If queue empty
+		item->next = item; //Loops to itself
+		item->prev = item;
+		head = item; //Items is new head
+	}
+	item->prev = head->prev; //Tail is previous of newly added element;
+	head->prev->next = item; //New item is next element of tail;
+	item->next = head; //Current head becomes next element of item;
+	head = item; //Item becomes new head;
+}
+
+element *delQueue(element *head) {
+	element *deletedItem = head;
+	element *temp = head;
+	if (head == NULL || head->next == NULL) { //Must have at at least two elements
+		rotateQueue(head);
+	}
+	free(temp);
+	return deletedItem;
+}
+
+//Move head pointer to next element
+void rotateQueue(element *head) {
+	head->next->prev = head->prev; //Next element's previous is now tail
+	head->prev->next = head->next; //Tail's next element is next element of head
+	head = head->next;
 }

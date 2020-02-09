c008347ef8c78710f1dde5ed363a20a390b68a39
Adam Hardy <ahardy313@gmail.com>
2020-02-09 11:22:12 -0700

    emptyQueue() added to master branch. Changes to rotateQueue() and delQueue()

--- Project1/proj_1.c
+++ Project1/proj_1.c
@@ -1,5 +1,8 @@
 #include "q.h"
 
+/*
+Prints all elements in queue
+*/
 void printQueue(element **head) {
 	element *tempHead = *head;
 	element *tempQueue = *head;
@@ -15,20 +18,77 @@ void printQueue(element **head) {
 	printf("\n"); 
 }
 
+/*
+This test function deletes all items from a queue.
+Prints payload of each item deleted upon each deletion,
+followed by values of remaining payloads in the queue.
+*/
+void emptyQueue(element **head) {
+	element *tempHead = *head;
+	element *tempQueue = *head;
+
+	int returnedValue = 0;
+
+	//Delete first queue item if queue is not empty.
+	if (head != NULL) {
+
+		element *temp = delQueue(&tempQueue);
+		returnedValue = (int)temp->payload;
+		printf("Value returned is %d\n", +returnedValue);
+		printf("Remaining payload values in the queue are: \n");
+		printQueue(&tempQueue);
+
+
+		//Initiate Boolean value for an empty queue to -1(not empty).
+		int queueEmpty = -1;
+
+		//Delete remaining queue items.
+		while (queueEmpty != 1)
+		{
+			element *temp = delQueue(&tempQueue);
+			returnedValue = (int)temp->payload;
+
+			//Test if the temp's next value is itself.
+			if (temp->next == temp) {
+
+				queueEmpty = 1;
+			}
+			printf("Value returned is %d\n", +returnedValue);
+
+			if (queueEmpty != 1)
+			{
+
+				tempQueue = tempQueue->next;
+
+				printf("Remaining payload values in the queue are: \n");
+				printQueue(&tempQueue);
+			}
+
+			if (queueEmpty == 1) {
+
+				printf("There are no remaining values in the queue.\n");
+				printf("\n");
+			}
+
+		}
+	}
+}
+
 
 int main() {
 	element *q1 = newItem();
 	initQueue(&q1);
 	element *item1 = newItem();
-	item1->payload = 5;
-	addQueue(&q1, item1);
 	element *item2 = newItem();
-	item2->payload = 20;
-	addQueue(&q1, item2);
 	element *item3 = newItem();
+	item1->payload = 5;
+	item2->payload = 20;
 	item3->payload = 50;
+	addQueue(&q1, item1);
+	addQueue(&q1, item2);
 	addQueue(&q1, item3);
 	printQueue(&q1);
+	
 	element *q2 = newItem();
 	initQueue(&q2);
 	element *item4 = newItem();
@@ -41,6 +101,33 @@ int main() {
 	addQueue(&q2, item5);
 	addQueue(&q2, item6);
 	printQueue(&q2);
+
+	element *q3;
+	initQueue(&q3);
+	element *item7 = newItem();
+	element *item8 = newItem();
+	element *item9 = newItem();
+	element *item10 = newItem();
+	item7->payload = -3;
+	item8->payload = -2;
+	item9->payload = -1;
+	item10->payload = 0;
+	addQueue(&q3, item7);
+	addQueue(&q3, item8);
+	addQueue(&q3, item9);
+	addQueue(&q3, item10);
+
+	printQueue(&q3);
+	
+	//test deleting queues
+	printf("Deleting all items from q3:\n");
+	emptyQueue(&q3);
+
+	printf("Deleting all items from q2:\n");
+	emptyQueue(&q2);
+
+	printf("Deleting all items from q1:\n");
+	emptyQueue(&q1);
 
 	return 0;
 }
--- Project1/q.h
+++ Project1/q.h
@@ -1,28 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//Queue item structure. Contains a payload and next and previous pointers
+/*
+Defines structure for elements within queue
+*/
 typedef struct element {
 	int payload;
 	struct element *next;
 	struct element *prev;
 } element;
 
-//Creates a new item. Allocates space for the item and returns it.
+/*
+Creates a new item. Allocates space for item and sets default
+values
+*/
 element* newItem() {
 	struct element *newItem = (element*)malloc(sizeof(element));
 	newItem->next = NULL;
 	newItem->prev = NULL;
-	//newItem->payload = 0;
 	return newItem;
 }
-
-//Initializes a queue
+/*
+Initializes queue. head is NULL when first initalizing.
+*/
 void initQueue(element **head) {
 	*head = NULL; //Head is set to NULL since queue is empty
 }
 
-//Add item to front of queue
+/*
+Takes in a pointer to the front of the queue and a new element.
+Determins whether or not the queue is empty and adds items accordingly.
+*/
 void addQueue(element **head, element *item) {
 	if (*head == NULL) { //If queue empty
 		item->next = item; //Loops to itself
@@ -35,26 +43,35 @@ void addQueue(element **head, element *item) {
 		tempHead->prev->next = item; //New item is next element of tail;
 		tempHead->prev = item; //Previous of original head is new item
 		item->next = *head; //Current head becomes next element of item;
-		*head = item; //Item becomes new head;
 	}
 }
 
-//Move head pointer to next element
+/*
+Moves head pointer to the next element
+*/
 void rotateQueue(element **head) {
-	element *tempHead = head;
+	element *tempHead = *head;
 	tempHead->next->prev = tempHead->prev; //Next element's previous is now tail
 	tempHead->prev->next = tempHead->next; //Tail's next element is next element of head
-	head = tempHead->next;
+	*head = tempHead->next;
 }
 
-element* delQueue(element *head) {
-	element *deletedItem = head;
-	element *temp = head;
-	if (head == NULL || head->next == NULL) { //Must have at at least two elements
-		rotateQueue(head);
+/*
+Deletes the element at the head of the queue and calls rotateQueue()
+if there are two or more elements.
+*/
+element* delQueue(element **head) {
+	//element *deletedItem = *head;
+	element *tempQueue = *head;
+	element *tempHead = *head;
+	if (tempHead != NULL && tempHead->next != NULL) { //Must have at at least two elements
+		rotateQueue(&tempQueue);
+		*head = tempQueue;
 	}
-	free(temp);
-	return deletedItem;
+	else {
+		*head = NULL;
+	}
+	return tempHead;
 }
 
 
